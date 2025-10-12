4517b7237f3d160431cdff4f97f137f38737a955
Rybakovskiy Nikita Alekseevich <nrybakovskiy@ozon.ru>
2025-10-12 15:30:57 +0300

    slices

--- slices_and_arrays/homework_test.go
+++ slices_and_arrays/homework_test.go
@@ -12,34 +12,89 @@ import (
 type CircularQueue struct {
 	values []int
 	// need to implement
+	size, rear, front int // two pointers
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	return CircularQueue{
+		values: make([]int, size),
+		// need to implement
+		size:  size,
+		rear:  -1,
+		front: -1,
+	}
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	// need to implement
+	if q.Full() {
+		return false
+	}
+
+	if q.Empty() {
+		q.front = 0
+	}
+
+	q.rear = (q.rear + 1) % q.size
+	q.values[q.rear] = value
+
+	return true
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	// need to implement
+	if q.Empty() {
+		return false
+	}
+
+	// for the last element
+	if q.rear == q.front {
+		q.rear = -1
+		q.front = -1
+		return true
+	}
+
+	q.front = (q.front + 1) % q.size
+
+	return true
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.front == -1 {
+		return -1
+	}
+	return q.values[q.front] // need to implement
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.rear == -1 {
+		return -1
+	}
+	return q.values[q.rear] // need to implement
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	// need to implement
+	return q.rear == -1 && q.front == -1
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	// need to implement
+	return q.front == q.size-1 || q.front == (q.rear+1)%q.size
+}
+
+func TestFull(t *testing.T) {
+	queue := NewCircularQueue(3)
+	queue.values = []int{1, 2, 3}
+	queue.front = 0
+	queue.rear = 2
+
+	assert.True(t, queue.Full())
+
+	queue.front = 1
+	queue.rear = 0
+
+	assert.True(t, queue.Full())
 }
 
 func TestCircularQueue(t *testing.T) {
@@ -83,4 +138,14 @@ func TestCircularQueue(t *testing.T) {
 
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())
+}
+
+func TestCircularQueueAdditional(t *testing.T) {
+	const queueSize = 3
+	queue := NewCircularQueue(queueSize)
+	queue.values = []int{1, 0, 3}
+	queue.front = 2
+	queue.rear = 0
+	assert.True(t, queue.Pop())
+	assert.Equal(t, 0, queue.front)
 }
